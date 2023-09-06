40995e170063ea62c3799426465513a022a51eac
Rishab8077 <rishabbothra56@gmail.com>
2023-09-06 23:28:32 +0545

    notebook

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.textsummarizer.logging import logger
+
+logger.info("WELCOME")
---
+++ src/textsummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+#in constants we call the config.yaml and params.yaml
+from pathlib import Path
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH =Path("params.yaml")
