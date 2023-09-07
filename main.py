8c2bce378fcfffddf19c7c0a010164aaf1658c5b
Rishab8077 <rishabbothra56@gmail.com>
2023-09-07 13:35:52 +0545

    pipeline

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from src.textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textsummarizer.logging import logger
 
-logger.info("WELCOME")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
